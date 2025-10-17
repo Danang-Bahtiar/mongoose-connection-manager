ef5f90a1bfc58787b1d111ddfc04d268c7645eeb
Danang-Bahtiar <dan.dev1903@gmail.com>
2025-10-17 17:06:24 +0700

    [Friday, 17-Oct-2025] - Fixed SchemaModule definition

--- src/core/schema/schema.module.ts
+++ src/core/schema/schema.module.ts
@@ -1,13 +1,12 @@
 import { Connection, Model, Schema } from "mongoose";
 
 class SchemaModule {
-  // protected model: Model<any>;
-  // connection: Connection, modelName: string, modelSchema: Schema
+  protected model: Model<any>;
 
-  constructor() {
-    // this.model = connection.models[modelName]
-    //   ? connection.models[modelName]
-    //   : connection.model(modelName, modelSchema);
+  constructor(connection: Connection, modelName: string, modelSchema: Schema) {
+    this.model = connection.models[modelName]
+      ? connection.models[modelName]
+      : connection.model(modelName, modelSchema);
   }
 }
 
